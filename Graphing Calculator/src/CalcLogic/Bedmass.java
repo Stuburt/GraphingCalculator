b29900fa83ccdb2201934eaa08c070c88b99e892
Stuart McFarlane <stuby.stu@gmail.com>
2016-04-15 09:18:41 -0400

    Test

---
+++ Graphing Calculator/src/CalcLogic/Bedmass.java
@@ -0,0 +1,9 @@
+package CalcLogic;
+
+public class Bedmass {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ Graphing Calculator/src/CalcLogic/MainLogic.java
@@ -0,0 +1,22 @@
+package CalcLogic;
+
+public class MainLogic {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
